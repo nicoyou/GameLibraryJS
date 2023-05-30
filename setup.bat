d0b2478c0969f30d972a7249f725745ec41300ca
羽咲蒼 <50352295+nicoyou@users.noreply.github.com>
2023-05-30 10:42:29 +0900

    v1.0.0
    Initial commit
    
    add: typescript 環境をセットアップするバッチファイルを追加
    
    add: ドキュメントを生成するバッチファイルを追加
    
    add: typescript を javascript にコンパイルするバッチファイルを追加

---
+++ setup.bat
@@ -0,0 +1,9 @@
+call npm install --save-dev typescript
+
+call npm install --save-dev eslint
+call npm install --save-dev @typescript-eslint/eslint-plugin
+call npm install --save-dev @typescript-eslint/parser
+call npm install --save-dev eslint-plugin-import
+call npm install --save-dev typedoc
+
+pause
